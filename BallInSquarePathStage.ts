43146878f68697f4d997ca45687fe6d502f6abb4
Anwesh43 <anweshthecool0@gmail.com>
2019-01-06 15:04:42 +0530

    adding BSPNode class

--- BallInSquarePathStage.ts
+++ BallInSquarePathStage.ts
@@ -137,3 +137,47 @@ class Animator {
         }
     }
 }
+
+class  BSPNode {
+    next : BSPNode
+    prev : BSPNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new BSPNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawBSPNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BSPNode {
+        var curr : BSPNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
