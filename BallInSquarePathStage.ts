0ec3d1fce08d08e1d79a6efc1b86597f480160ea
Anwesh43 <anweshthecool0@gmail.com>
2019-01-06 12:00:17 +0530

    adding constants

---
+++ BallInSquarePathStage.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+const paths : number = 4
+const sizeFactor : number = 3
+const rFactor : number = 5
+const strokeFactor : number = 90
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const foreColor : string = "#673AB7"
+const backColor : string = "#212121"
