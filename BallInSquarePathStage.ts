965ef2c5c7fa6c9f9377b206d9dbc51f2b188652
Anwesh43 <anweshthecool0@gmail.com>
2019-01-06 14:25:40 +0530

    adding drawBSPNode drawing method

--- BallInSquarePathStage.ts
+++ BallInSquarePathStage.ts
@@ -25,3 +25,43 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
 const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
 }
+
+const drawBSPNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = w / (nodes + 1)
+    const size : number = gap / sizeFactor
+    const r : number = size / rFactor
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.lineCap = 'round'
+    context.strokeStyle = foreColor
+    context.fillStyle = foreColor
+    context.save()
+    context.translate((i + 1) * gap, h/2)
+    var deg : number = 0
+    var sc : number = 0
+    for (var j = 0; j < paths; j++) {
+        context.save()
+        context.rotate(Math.PI/2 * j)
+        context.beginPath()
+        context.moveTo(size, size)
+        context.lineTo(-2 * size, 0)
+        context.stroke()
+        context.restore()
+        var scj : number = divideScale(sc, j, paths)
+        if (scj > 0 && scj <= 1) {
+            sc = scj
+        } else if (scj == 1) {
+            sc = 1
+        }
+        deg += (Math.PI / 2) * Math.floor(sc)
+    }
+    var x : number = size - 2 * size * sc
+    context.save()
+    context.rotate(deg)
+    context.beginPath()
+    context.arc(x, size, r, 0, 2 * Math.PI)
+    context.fill()
+    context.restore()
+    context.restore()
+}
